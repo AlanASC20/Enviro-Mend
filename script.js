7272e5bf6444efec8b0442031aa989d4e19ad8ac
Alan Espinal <alanedespinal@gmail.com>
2020-08-10 14:33:30 -0400

    starting to code

--- script.js
+++ script.js
@@ -1,28 +1,40 @@
-let url = "https://cors-anywhere.herokuapp.com/https://swe-endangered-animals.appspot.com/single_country_data/";
+// let request = new XMLHttpRequest();
 
-fetch(url)
-    .then(function(response){
-        return response.json(); 
-    })
-    .then(function(myJson){
-        displayData(myJson);   
-    })
+// request.open('GET', 'https://cors-anywhere.herokuapp.com/https://swe-endangered-animals.appspot.com/single_country_data/');
+
+// request.onreadystatechange = function () {
+//   if (this.readyState === 4) {
+//     console.log('Status:', this.status);
+//     console.log('Headers:', this.getAllResponseHeaders());
+//     console.log('Body:', this.responseText);
+//   }
+// };
+
+// request.send();
+
+// fetch("https://cors-anywhere.herokuapp.com/https://swe-endangered-animals.appspot.com/single_country_data/?country_name=Azerbaijan")
+//     .then(response =>
+//         response.json()
+//     )
+//     .then(json =>
+//         console.log(json)   
+//     );
     
-    function displayData(countryData){
-        let country = countryData.name;
-        let animals = countryData.assoc_animals;
-        let habitats = countryData.assoc_habitats;
+//     function displayData(countryData){
+//         let country = countryData.name;
+//         let animals = countryData.assoc_animals;
+//         let habitats = countryData.assoc_habitats;
 
-        let c = document.createElement('p');
-        let a = document.createElement('p');
-        let h = document.createElement('p');
+//         let c = document.createElement('p');
+//         let a = document.createElement('p');
+//         let h = document.createElement('p');
 
-        c.innerHTML = country;
-        a.innerHTML = animals;
-        h.innerHTML = habitats;
+//         c.innerHTML = country;
+//         a.innerHTML = animals;
+//         h.innerHTML = habitats;
         
-        document.body.appendChild(c);
-        document.body.appendChild(a);
-        document.body.appendChild(h);
+//         document.body.appendChild(c);
+//         document.body.appendChild(a);
+//         document.body.appendChild(h);
         
-    }
+//     }
